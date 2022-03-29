9c54c27f1633c2bec66c40e9e43357e23793acbd
6levi9 <robertojr.rga@gmail.com>
2022-03-29 08:40:07 -0400

    binarios

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,11 +1,25 @@
 function BinarioADecimal(num) {
   // tu codigo aca
+  var suma = 0
+  var numeroRevertido = num.split('').reverse().join('')
 
+
+  for (let i = 0; i < numeroRevertido.length; i++) {
+    suma = suma + numeroRevertido[i] * 2 ** i;
+  }
+  return suma;
 }
+
+
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let binario = "";
+  while( num != 0 ){          
+  binario = num % 2 + binario  
+  num = Math.floor(num/2); 
+  }
+  return binario; 
 }
 
 
